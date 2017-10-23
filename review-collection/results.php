f9fae270ceb5ee6d90dd6e69ca6561b605b124a7
Zach Walsh <ztwalsh@gmail.com>
2017-10-23 10:29:07 -0500

    tweaks for the test

--- review-collection/results.php
+++ review-collection/results.php
@@ -7,15 +7,19 @@
     $merchant_group_id = NULL;
   }
 
-  $query = 'SELECT * FROM images WHERE merchant_group_id = '.$merchant_group_id.'';
+  $image_query = 'SELECT * FROM images WHERE merchant_group_id = '.$merchant_group_id.'';
+  $caption_query = ' AND (caption is null or caption = "")';
+  $review_query = 'SELECT * FROM reviews WHERE merchant_group_id = '.$merchant_group_id.'';
   $remove = ' AND merchant_user_email NOT IN ("ztwalsh@gmail.com","zach.walsh@powerreviews.com","sara.rossio@powerreviews.com","rachel.bentley@powerreviews.com")';
-  $cap = ' AND (caption is null or caption = "")';
-  $images = $mysqli->query($query.$remove);
-  $captions = $mysqli->query($query.$remove.$cap);
+  $images = $mysqli->query($image_query.$remove);
+  $reviews = $mysqli->query($review_query.$remove);
+  $captions = $mysqli->query($image_query.$remove.$caption_query);
 
   $image_count = mysqli_num_rows($images);
   $caption_count = mysqli_num_rows($captions);
+  $review_count = mysqli_num_rows($reviews);
   $caption_percentage = $caption_count/$image_count*100;
+  $caption_percentage = $review_count/$image_count*100;
 ?>
 
 <!DOCTYPE html>
@@ -37,8 +41,9 @@
     </section> -->
     <section>
   		<?php
-        echo '<p>Images: '.$image_count.'</p>';
-        echo '<p>Captions: '.$caption_count.' ('.$caption_percentage.'%)</p>';
+        echo '<p>Images: '.$image_count.'<br />';
+        echo '<p>Captions: '.$caption_count.' ('.$caption_percentage.'%)<br />';
+        echo '<p>Reviews: '.$review_count.' ('.$review_percentage.'%)</p>';
         while($image = $images->fetch_assoc()) {
           echo '<p>';
             echo '<img src="'.$image['file_name'].'" width="400" height="auto" /><br />';
